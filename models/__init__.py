73b9d4c7b4e6fea1201a67712cb2f665bad190e3
Manuel Marquez <buzondemam@gmail.com>
2017-11-06 01:27:14 -0400

    [ADD] Model stock.picking.charge.monarch.unattached.neck to record of charges by monarch unattached to neck.

--- models/__init__.py
+++ models/__init__.py
@@ -6,3 +6,4 @@ from . import charge_supplier_receipt
 from . import stock_picking
 from . import stock_picking_charge_other_supplier_box
 from . import stock_picking_charge_change_material
+from . import stock_picking_charge_monarch_unattached_neck
