ff0b8ed16a1a342ba620144e782c50088326a726
Manuel Marquez <buzondemam@gmail.com>
2017-12-04 13:19:31 -0400

    [IMP] Updates version to new release 9.0.1.0.1 in manifest file and CHANGELOG.

--- __openerp__.py
+++ __openerp__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Manager of charges to the suppliers on receipt of goods',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.0.1',
     'category': 'Stock',
     'author': 'Humanytek',
     'website': "http://www.humanytek.com",
